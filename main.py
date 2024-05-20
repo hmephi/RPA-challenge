f10a6477dcddf5b0c3c3b7c88fdba9e84bd7ba0a
hmephi <david@itonlinelearning.com>
2024-05-20 11:52:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 import os
 import re
-# import requests
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
